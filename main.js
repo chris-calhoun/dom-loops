4fbafe32b0ca3405c176be134dc29ee229822a24
Chris Calhoun <ctcalhoun2@gmail.com>
2020-07-21 20:01:41 -0500

    worked on loops

--- main.js
+++ main.js
@@ -1,56 +1,58 @@
-"use strict";
+// "use strict";
 
-// const ageCheck = (age) => {
-//   if (age < 21) {
-//     console.log("No drinks for you!");
-//   } else {
-//     console.log("PARTY!");
-//   }
+// // const ageCheck = (age) => {
+// //   if (age < 21) {
+// //     console.log("No drinks for you!");
+// //   } else {
+// //     console.log("PARTY!");
+// //   }
+// // };
+
+// // // ageCheck(12);
+// // // ageCheck(21);
+
+// // const lengthOutput = (string) => {
+// //   return string.length;
+// // };
+
+// // console.log(lengthOutput([1, 2, 3, 4, 5, 6, 7]));
+
+// // const quoteFinder = (testString) => {
+// //   return "winter is coming".indexOf(testString);
+// // };
+
+// // console.log(quoteFinder("is"));
+
+// const employee = {
+//   firstName: "Chris",
+//   lastName: "Calhoun",
+//   role: "student",
+//   accountNumber: "1-452654625",
+//   isManager: false,
+//   titles: ["Jr. Developer", "Bad Ass"],
+//   accolade: {
+//     title: "Employee of the Year",
+//     dateEarned: "1/2/2020",
+//     expires: "12/21/2020",
+//     isCool: true,
+//   },
 // };
 
-// // ageCheck(12);
-// // ageCheck(21);
+// // console.log(employee.firstName);
+// // const keyToCheck = "accolade";
 
-// const lengthOutput = (string) => {
-//   return string.length;
-// };
+// // console.log(employee[keyToCheck]["isCool"]);
 
-// console.log(lengthOutput([1, 2, 3, 4, 5, 6, 7]));
+// // Challenge #1
+// // const vipOrPeasant = (employeeName) => {
+// //   if (employeeName.firstName[0] === "M") {
+// //     employeeName.status = "vip";
+// //   } else {
+// //     employeeName.status = "peasant";
+// //   }
+// // };
 
-// const quoteFinder = (testString) => {
-//   return "winter is coming".indexOf(testString);
-// };
+// // vipOrPeasant(employee);
+// // console.log(employee.status);
 
-// console.log(quoteFinder("is"));
-
-const employee = {
-  firstName: "Chris",
-  lastName: "Calhoun",
-  role: "student",
-  accountNumber: "1-452654625",
-  isManager: false,
-  titles: ["Jr. Developer", "Bad Ass"],
-  accolade: {
-    title: "Employee of the Year",
-    dateEarned: "1/2/2020",
-    expires: "12/21/2020",
-    isCool: true,
-  },
-};
-
-// console.log(employee.firstName);
-// const keyToCheck = "accolade";
-
-// console.log(employee[keyToCheck]["isCool"]);
-
-// Challenge #1
-const vipOrPeasant = (employeeName) => {
-  if (employeeName.firstName[0] === "M") {
-    employeeName.status = "vip";
-  } else {
-    employeeName.status = "peasant";
-  }
-};
-
-vipOrPeasant(employee);
-console.log(employee.status);
+// //Challenge #2
